82c93c81c3eafe61dd625d80a31e6cdc9ecab1a5
chasechri <christensonchase451@gmail.com>
2022-07-27 11:50:24 -0500

    comments

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,12 +1,11 @@
 const router = require("express").Router();
-const { Post, User, Vote } = require("../../models");
+const { Post, User, Vote, Comment } = require("../../models");
 const sequelize = require("../../config/connection");
 
 // get all users
 router.get("/", (req, res) => {
   console.log("======================");
   Post.findAll({
-    // Query configuration
     attributes: [
       "id",
       "post_url",
@@ -21,6 +20,14 @@ router.get("/", (req, res) => {
     ],
     order: [["created_at", "DESC"]],
     include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
       {
         model: User,
         attributes: ["username"],
@@ -52,6 +59,14 @@ router.get("/:id", (req, res) => {
       ],
     ],
     include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
       {
         model: User,
         attributes: ["username"],
